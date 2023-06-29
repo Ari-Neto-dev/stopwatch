08cb757bc958862aecef02af1b7d39ba72ef4492
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-06-29 14:39:05 -0300

    styling in CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{Component} from 'react';
+import './style.css'
+class App extends Component{
+  render(){
+    return(
+      <div className='container'>
+        <img src={require('./assets/cronometro.png')} alt="" className='img'/>
+        <a>0.0</a>
+        <div className='areaButton'>
+          <a className='button'>VAI</a>
+          <a className='button'>LIMPAR</a>
+        </div>
+    
+      </div>
+    );
+  }
 }
-
 export default App;
